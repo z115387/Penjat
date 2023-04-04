1f8180e436fb893826215f7d2a20e27a8fd28d06
z115387 <123639024+z115387@users.noreply.github.com>
2023-04-04 09:49:32 +0200

    Add files via upload

--- Penjat.js
+++ Penjat.js
@@ -4,13 +4,9 @@
  * and open the template in the editor.
  */
 
-
-
         var vidas= 7;
-
-
-
-        var letra=["_", "_", "_","_", "_", "_","_"];
+        var encertades = [];
+        var fallades=["_", "_", "_","_", "_", "_","_"];
         
         var punts=0;
           // Lletra de paraules per al joc
@@ -22,47 +18,53 @@
           var aleatori = Math.floor(Math.random() * paraules.length);
           var paraula = paraules[aleatori];
           var pista =  pistes[paraulespistes[aleatori]];
-          var lletres = [];
-          for (var i = 0; i < paraula.length; i++) {
-            lletres[i] = "_";      
+          
+           for (var i = 0; i < paraula.length; i++) {
+            encertades[i] = "_";      
           }
           // document.getElementById("palabro").innerHTML = lletres;
 
         function Verificar(){
 
-			var lletra= document.getElementById("lletra").value;
+			var lletra = document.getElementById("lletra").value;
                         var pos = paraula.indexOf(lletra);
                         alert(paraula);
-			if((pos == -1)) {
-                                alert("has fallat");
+                        
+                        if ((encertades.indexOf(lletra) != -1) || (fallades.indexOf(lletra) != -1)) {
+                           alert("Lletra repetida");                        
+                        } else {
+                        
+                            if((pos == -1)) {
+                                    alert("has fallat");
 
-                                // letra=letra+" "+lletra;
-                                letra[7 - vidas]=lletra;
-				document.getElementById("letras").innerHTML=letra;
-                                
-				vidas= vidas-1;
-				document.getElementById("vidas").innerHTML=vidas;
+                                    // letra=letra+" "+lletra;
+                                    fallades[7 - vidas]=lletra;
+                                    document.getElementById("letras").innerHTML=fallades;
 
-			} else {
-                                alert("has encertat");
-                                
-				// lletres=lletres+" "+lletra;
-                                for (var i = pos; i < paraula.length; i++){
-                                    if (paraula[i] == lletra){
-                                        lletres[i]=lletra;
+                                    vidas= vidas-1;
+                                    document.getElementById("vidas").innerHTML=vidas;
+
+                            } else {
+                                    alert("has encertat");
+
+                                    // lletres=lletres+" "+lletra;
+                                    for (var i = pos; i < paraula.length; i++){
+                                        if (paraula[i] == lletra){
+                                            encertades[i]=lletra;
+                                        }
                                     }
-                                }
-				document.getElementById("palabro").innerHTML=lletres;
-			}
+                                    document.getElementById("palabro").innerHTML=encertades;
+                            }
 
-			if ((vidas==0)){
+                            if ((vidas==0)){
 
-			  window.alert("Has perdut");
+                              document.body.style.backgroundImage = "url('img/Jungle.png')";
+                              window.alert("Has perdut");
 
-			  lletra="";
+                              lletra="";
 
-			}
-
+                            }
+                        }
           
 		}
                 
